e3eccbf6ec51b6d796763e4262a02d06cf3426fd
sebastian.bombitzky <sebastian.bombitzky@exozet.com>
2020-05-08 16:42:43 +0200

    changed page title

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  chainWebpack: config => {
+    config
+    .plugin('html')
+    .tap(args => {
+      args[0].title = 'Curry & Fritz';
+      return args;
+    });
+  },
   pwa: {
     name: 'Curry & Fritz',
     short_name: 'Curry & Fritz',
